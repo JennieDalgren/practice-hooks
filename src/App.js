23e1e93eba6dab9e2abd8e875cc997b5e14df0bf
Jennie Dalgren <jennie.dalgren@gmail.com>
2019-09-11 18:22:56 +0200

    setup and test useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  const [userQuery, setUserQuery] = useState("")
+
+  const updateUserQuery = e => {
+    console.log("userQuery", userQuery)
+    setUserQuery(e.target.value)
+  }
+
+  const handleKeyPress = e => {
+    if (e.key === "Enter") searchQuery()
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, "_blank")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello Jennie</h1>
+      <div className='form'>
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

53f66b0506899c77d970be76f755f27aa1be7436
Jennie Dalgren <jennie.dalgren@gmail.com>
2019-09-14 23:02:47 +0200

    refactor with custom hook for dynamic transition

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from "react"
 import PICTURES from "./data/pictures"
+import { useDynamicTransition } from "./hooks"
 
 const SECONDS = 1000
 const minimumDelay = 1000
 const minimumIncrement = 1
 
 function Gallery() {
-  const [index, setIndex] = useState(0)
   const [delay, setDelay] = useState(3 * SECONDS)
   const [increment, setIncrement] = useState(1)
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex(storedIndex => (storedIndex + increment) % PICTURES.length) //If we're at the end, set the index to 0 i.e. length is 9. 9%9 is 0
-    }, delay)
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [delay, increment])
+  const index = useDynamicTransition({
+    delay,
+    increment,
+    length: PICTURES.length
+  })
 
   const updateDelay = event => {
     const delay = Number(event.target.value) * SECONDS
--- src/hooks.js
+++ src/hooks.js
@@ -11,3 +11,17 @@ export const useFetch = (url, initalValue) => {
 
   return result
 }
+
+export const useDynamicTransition = ({ increment, delay, length }) => {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(storedIndex => (storedIndex + increment) % length)
+    }, delay)
+
+    return () => clearInterval(interval)
+  }, [delay, increment])
+
+  return index
+}
